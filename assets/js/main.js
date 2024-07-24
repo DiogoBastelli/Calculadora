84da8024bc12678761ec4be01c6cf021c29ebca1
DiogoBastelli <90928308+DiogoBastelli@users.noreply.github.com>
2024-07-23 23:54:52 -0300

    atualizao do codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,71 +1,44 @@
-function criaCalculadora(){
-    return{
-        display: document.querySelector('.display'),
+function Calculadora(){
 
-        realizaConta(){
-            let conta = this.display.value;
+    this.display = document.querySelector('.display');
 
-            try{
-                conta = eval(conta)
-                if(!conta){
-                    alert('conta invalida');
-                    return;
-                }
-                this.display.value = String(conta);
-            } catch(e){
+    this.inicia = ()=>{
+        this.cliqueBotoes();
+    };
+
+    this.cliqueBotoes = () => {
+        document.addEventListener('click' , e =>{
+            const el = e.target;
+
+            if(el.classList.contains('btn-num')) this.btnParaDisplay(el.innerText);
+
+            if(el.classList.contains('btn-clear')) this.clearDisplay();
+
+            if(el.classList.contains('btn-del')) this.apaga1();
+
+            if(el.classList.contains('btn-eq')) this.realizaConta();
+        });
+    };
+
+    this.btnParaDisplay = (valor)=> this.display.value += valor;
+    this.apaga1 = () => this.display.value =  this.display.value.slice(0,-1);
+    this.clearDisplay = () => this.display.value = '';
+
+    this.realizaConta = () =>{
+        try{
+            const conta = eval(this.display.value);
+
+            if(!conta){
                 alert('conta invalida');
                 return;
             }
-        },
-
-        apaga1(){
-            this.display.value =  this.display.value.slice(0,-1);
-        },
-
-        clearDisplay(){
-            this.display.value = ''; 
-        },
-
-        inicia(){
-            this.cliqueBotoes();
-            this.pressionaEnter();
-        },
-
-        pressionaEnter(){
-            this.display.addEventListener('keyup' , e =>{
-                if(e.keyCode === 13){
-                    this.realizaConta();
-                }
-            })
-        },
-
-        cliqueBotoes(){
-            document.addEventListener('click' , function(e){
-                const el = e.target;
-
-                if(el.classList.contains('btn-num')){
-                    this.btnParaDisplay(el.innerText);
-                }
-
-                if(el.classList.contains('btn-clear')){
-                    this.clearDisplay();
-                }
-
-                if(el.classList.contains('btn-del')){
-                    this.apaga1();
-                }
-
-                if(el.classList.contains('btn-eq')){
-                    this.realizaConta();
-                }
-            }.bind(this));
-        },
-        btnParaDisplay(valor){
-            this.display.value += valor;
+            this.display.value = String(conta);
+        } catch(e){
+            alert('conta invalida');
+            return;
         }
-
     };
-}
+};
 
-const calculadora = criaCalculadora();
+const calculadora = new Calculadora();
 calculadora.inicia();
